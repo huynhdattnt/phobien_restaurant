7a7cd82aa94bc941247becbdad7e5cda534528ea
datht <dat.huynh@commgate.net>
2017-02-19 14:47:39 +0700

    add search for finding foods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def search
+  	puts "13333333333"
+  end
 end
--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -5,4 +5,7 @@ class FoodItem < ApplicationRecord
   def image_url_or_default
   	image_url.presence || "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSBKrTFYWPlRit2ywyQgWgoHdT92swDqEeeGpdSEkGX4rv3X24j"
   end
+  def self.search(search)
+	  where("LOWER(name) LIKE ?", "%#{search.downcase}%")
+	end
 end
